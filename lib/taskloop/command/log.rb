733be91888203cdd7e6cbd8ce013ce209b7a5d24
baocq <baochuquan@163.com>
2023-07-18 22:24:17 +0800

    feat: add implement for taskloop log

--- lib/taskloop/command/log.rb
+++ lib/taskloop/command/log.rb
@@ -35,25 +35,32 @@ module TaskLoop
     def run
       super
       if @task_name
-        check_log_for_task(@task_name)
+        check_log_of_task(@task_name)
+        return
+      end
+
+      if @cron
+        check_log_of_cron
+        return
       end
     end
 
-    def check_log_for_task(name)
+    def check_log_of_task(name)
       proj_cache_dirs = Dir.entries(taskloop_cache_dir)
       proj_cache_dirs.each do |dir|
         if dir == "." or dir == ".."
           next
         end
 
+        found = false
         proj_cache_dir = File.join(taskloop_cache_dir, dir)
         log_files = Dir.entries(proj_cache_dir)
         log_files.each do |file|
           if "#{name.sha1}_log" == file
+            found = true
             task_path = File.join(proj_cache_dir, "#{name.sha1}_log")
             puts "=============================".ansi.blue
             puts "Log of <Task.name: #{name}>: ".ansi.blue
-            puts ""
             File.open(task_path).each_line do |line|
               puts line
             end
@@ -61,7 +68,24 @@ module TaskLoop
             puts ""
           end
         end
+
+        unless found
+          puts "Warning: log of <Task.name: #{name}> not exist. Please check if the name of task is correct.".ansi.yellow
+          puts ""
+        end
       end
+    end
+
+    def check_log_of_cron
+      puts "=============================".ansi.blue
+      puts "Log of cron: ".ansi.blue
+      puts ""
+      log_file = File.open(taskloop_cron_log_path, "r")
+      log_file.each_line do |line|
+        puts line
+      end
+      puts "=============================".ansi.blue
+      puts ""
     end
   end
 end
