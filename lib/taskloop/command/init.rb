10b55f7e440cc75ab08b223625a943ef7636925d
baocq <baochuquan@163.com>
2023-08-01 23:26:33 +0800

    fix: taskloop init

--- lib/taskloop/command/init.rb
+++ lib/taskloop/command/init.rb
@@ -26,15 +26,18 @@ module TaskLoop
 
 TaskLoop::Task.new do |t|
   t.name        = 'TODO: task name. <Required>'
-  t.path        = 'TODO: task job path. For exmaple, t.path = "./Job.sh". <Required>'
+  t.path        = 'TODO: task job path. For example, t.path = "./Job.sh". <Required>'
+  t.week        = 'TODO: week rule. <Optional>'
   t.year        = "TODO: year rule. <Optional>"
   t.month       = "TODO: month rule. <Optional>"
   t.day         = "TODO: day rule. <Optional>"
   t.hour        = "TODO: hour rule. <Optional>"
   t.minute      = "TODO: minute rule. <Optional>"
+  t.time        = "TODO: time list rule. <Optional>"
+  t.date        = "TODO: date list rule. <Optional>"
   t.loop        = "TODO: loop count. <Optional>"
-  t.start_point = "TODO: start point boundary rule. <Optional"
-  t.end_point   = "TODO: start point boundary rule. <Optional"
+  t.start_point = "TODO: start point boundary rule. <Optional>"
+  t.end_point   = "TODO: end point boundary rule. <Optional>"
 end
       DESC
       file.puts content
